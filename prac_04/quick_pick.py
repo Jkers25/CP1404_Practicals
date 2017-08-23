caca914cf66f9ba52c65b2088975e87ba92fa27c
Jake Kerswell <Jake.Kerswell@my.jcu.edu.au>
2017-08-23 15:24:43 +1000

    Update prac 4

--- prac_04/quick_pick.py
+++ prac_04/quick_pick.py
@@ -1,15 +1,19 @@
 import random
-quick_picks = []
+
+MIN_NUMBER = 1
+
+MAX_NUMBER = 46
+
 number_of_quick_picks = int(input('Number of quick picks: '))
 for i in range(number_of_quick_picks):
-    for j in range(6):
-        random_number = random.randint(1,46)
-        while random_number in quick_picks:
-            random_number = random.randint(1, 46)
-        quick_picks.append(random_number)
-        quick_picks.sort()
-    for k in quick_picks:
-        print('{:2}'.format(k), end=" ")
-    print()
     quick_picks = []
+    for j in range(6):
+        random_number = random.randint(MIN_NUMBER, MAX_NUMBER)
+        while random_number in quick_picks:
+            random_number = random.randint(MIN_NUMBER, MAX_NUMBER)
+        quick_picks.append(random_number)
+    quick_picks.sort()
+    for quick_pick in quick_picks:
+        print('{:2}'.format(quick_pick), end=" ")
+    print()
 
