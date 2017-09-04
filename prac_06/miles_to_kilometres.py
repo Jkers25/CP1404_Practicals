7522dc3d69adf2952fe0bc2fbf232c2b32b71813
Jake Kerswell <Jake.Kerswell@my.jcu.edu.au>
2017-09-04 16:11:51 +1000

    Add prac_06 except final task

---
+++ prac_06/miles_to_kilometres.py
@@ -0,0 +1,44 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+__author__ = 'Jake Kerswell'
+
+
+class miles_to_kilometres(App):
+    def build(self):
+        self.title = "Miles to Kilometres"
+        self.root = Builder.load_file('miles_to_kilometres.kv')
+
+    def handle_calculate_miles_to_kilometres(self, miles):
+        miles = test(miles)
+        kilometres = miles * 0.621371
+        self.root.ids.output_label.text = str(kilometres)
+        return self.root
+
+    def handle_up(self, miles):
+        miles = test(miles)
+        miles += 1
+        self.root.ids.miles_input.text = str(miles)
+
+    def handle_down(self, miles):
+        miles = test(miles)
+        miles -= 1
+        print(miles)
+        self.root.ids.miles_input.text = str(miles)
+
+
+def test(user_input):
+    try:
+        if user_input != '':
+            miles = int(user_input)
+            print(int(user_input))
+        else:
+            miles = 0
+    except ValueError:
+            miles = 0
+
+    return miles
+
+
+miles_to_kilometres().run()
